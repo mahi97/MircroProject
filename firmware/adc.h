834356ae194732006d0e5940c6faa01664514ff9
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2019-01-23 15:06:53 +0330

    add adc init

--- firmware/adc.h
+++ firmware/adc.h
@@ -7,7 +7,14 @@
 
 
 void adc_init() {
-
+	ADCSRA = (1 << ADEN)  // Enable the ADC
+		   | (1 << ADIE)  //interrupt feature
+		   | (1 << ADATE) // Enable auto trigger
+		   | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // set the ACD clock pre-scalar to clk/128
+	ADMUX = (1 << REFS0) | (1 << REFS1) // Select internal 2.56V as Vref,
+	      | (1 << ADLAR);   // left justify data registers
+	      //select ADC0 as input channel
+	SFIOR &= (0x0F); // Free Running and Not High Speed Mode
 }
 
 #endif __ADC_HEADER__
